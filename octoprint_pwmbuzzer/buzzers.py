0486a5018ce422a20b13911d8ea9422880034cc4
Matt Bielich <mbielich@gmail.com>
2022-04-01 21:09:57 -0700

    #2: queue and handle M300 commands async

---
+++ octoprint_pwmbuzzer/buzzers.py
@@ -0,0 +1,96 @@
+from abc import ABC, abstractmethod
+import logging
+
+try:
+    import RPi.GPIO as GPIO
+    GPIO_AVAILABLE = True
+except ImportError:
+    GPIO_AVAILABLE = False
+
+class Buzzer(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def is_enabled():
+        pass
+
+    @abstractmethod
+    def start(self, frequency):
+        pass
+
+    @abstractmethod
+    def stop(self):
+        pass
+
+class HardwareBuzzer(Buzzer):
+    def __init__(self, enabled, pin, duty_cycle):
+        self._logger = logging.getLogger(__name__+"."+self.__class__.__name__)
+
+        self.pwm = None
+        self.set_settings(enabled, pin, duty_cycle)
+
+    def Available():
+        return GPIO_AVAILABLE
+
+    def set_settings(self, enabled, pin, duty_cycle):
+        if enabled is not None:
+            self._enabled = enabled
+        if pin is not None:
+            self._pin = pin
+        if duty_cycle is not None:
+            self._duty_cycle = duty_cycle
+
+        self._logger.info("HardwareBuzzer set to enabled {self._enabled} pin {self._pin} with duty cycle {self._duty_cycle}".format(**locals()))
+
+    def is_enabled(self):
+        return HardwareBuzzer.Available() and self._enabled
+
+    def start(self, frequency):
+        if not self.is_enabled():
+            return
+
+        self._logger.debug("🎵 starting tone... {frequency}Hz (using BCM pin {self._pin} at {self._duty_cycle}% duty cycle)".format(**locals()))
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self._pin, GPIO.OUT)
+        self.pwm = GPIO.PWM(self._pin, frequency)
+        self.pwm.start(self._duty_cycle)
+
+    def stop(self):
+        # Stop hardware tone regardless of feature enablement (don't leave a tone running)
+        if not HardwareBuzzer.Available():
+            return
+
+        if self.pwm is not None:
+            self.pwm.stop()
+            self.pwm = None
+            GPIO.cleanup()
+        elif self._enabled:
+            self._logger.warn("Hardware buzzer is enabled, but reference to PWM buzzer wasn't found when trying to stop the tone.")
+
+class SoftwareBuzzer(Buzzer):
+    def __init__(self, messageFunc, enabled):
+        self._logger = logging.getLogger(__name__+"."+self.__class__.__name__)
+
+        self._sendMessageImplementation = messageFunc
+        self.set_settings(enabled)
+
+    def set_settings(self, enabled):
+        if enabled is not None:
+            self._enabled = enabled
+
+        self._logger.info("SoftwareBuzzer set to enabled {self._enabled}".format(**locals()))
+
+    def is_enabled(self):
+        return self._enabled
+
+    def start(self, frequency):
+        self._sendMessageImplementation({
+            "action": "software_tone_start",
+            "frequency": frequency,
+        })
+
+    def stop(self):
+        self._sendMessageImplementation({
+            "action": "software_tone_stop",
+        })
---
+++ octoprint_pwmbuzzer/tones.py
@@ -0,0 +1,75 @@
+from enum import Enum
+from queue import SimpleQueue
+from threading import Thread
+import time
+import logging
+
+INTER_NOTE_PAUSE_DURATION = 0.01
+
+class ToneCommand(Enum):
+    STOP = 0
+    START = 1
+    PLAY = 2
+    REST = 3
+
+class Tone():
+    def __init__(self, command, buzzers = [], frequency = None, duration = None):
+        self._logger = logging.getLogger(__name__+"."+self.__class__.__name__)
+        self.command = command
+        self.buzzers = buzzers
+        self.frequency = frequency
+        self.duration = duration
+
+    def exec(self):
+        if len(self.buzzers) < 1:
+            self._logger.warn("{0} executed with no attached buzzers, ignoring.".format(self))
+            return
+
+        if self.command is ToneCommand.START:
+            for buzzer in self.buzzers:
+                buzzer.start(self.frequency)
+        elif self.command is ToneCommand.STOP:
+            for buzzer in self.buzzers:
+                buzzer.stop()
+        elif self.command is ToneCommand.PLAY:
+            for buzzer in self.buzzers:
+                buzzer.start(self.frequency)
+            time.sleep(self.duration / 1000)
+            for buzzer in self.buzzers:
+                buzzer.stop()
+
+            # Pause briefly between notes
+            time.sleep(INTER_NOTE_PAUSE_DURATION)
+        elif self.command is ToneCommand.REST:
+            time.sleep(self.duration / 1000)
+
+    def __repr__(self):
+        if self.command in [ToneCommand.START, ToneCommand.PLAY]:
+            return "%s (%s %.3fHz)" % (self.__class__.__name__, self.command.name, self.frequency)
+        else:
+            return "%s (%s)" % (self.__class__.__name__, self.command.name)
+
+class ToneQueue():
+    def __init__(self):
+        self._logger = logging.getLogger(__name__+"."+self.__class__.__name__)
+        self._queue = SimpleQueue()
+        self._thread = None
+
+    def add(self, tone):
+        self._logger.debug("adding to the queue: {0}".format(tone))
+        self._queue.put(tone)
+        self._runQueue()
+
+    def _runQueue(self):
+        if self._thread is not None and self._thread.is_alive():
+            return
+
+        self._thread = Thread(target=self._worker)
+        self._thread.start()
+        self._logger.debug("forked a thread: {0}".format(self._thread.ident))
+
+    def _worker(self):
+        while not self._queue.empty():
+            tone = self._queue.get()
+            self._logger.debug("pulled from the queue to be executed: {0}".format(tone))
+            tone.exec()
